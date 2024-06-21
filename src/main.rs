5b2a13153b57f9bf2b65456abb3cba7bf3465bfb
armansajedi944 <armansajedi944@gmail.com>
2024-06-21 06:16:50 -0700

    generate documentation for our project

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+
+
+/// A favorite color.
+
+enum Color {
+    Red,
+    Blue,
+}
+
+/// A prece of mail
+struct Mail{
+
+    /// The destination address.
+
+    address : String,
+
+}
+/// Add Two numbers together.
+fn add(a:i32,b:i32)->i32{
+    a+b
+}
+fn main() {
+}
